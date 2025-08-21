5a7e947fc8deb91c89754ba484c759357c5dc451
Luigi <luigi.mendes.cabrini@outlook.com>
2025-08-20 21:26:36 -0300

    Adicionar arquivos de projeto.

---
+++ RayCaster/Program.cs
@@ -0,0 +1,25 @@
+﻿using Raylib_cs;
+
+class Program
+{
+
+    static void Main()
+    {
+
+
+        const int screenWidth = 1280;
+        const int screenHeight = 720;
+
+        Raylib.InitWindow(screenWidth, screenHeight, "raylib-cs • janela cinza");
+        Raylib.SetTargetFPS(60);
+
+        while (!Raylib.WindowShouldClose())
+        {
+            Raylib.BeginDrawing();
+            Raylib.ClearBackground(Color.Gray); 
+            Raylib.EndDrawing();
+        }
+
+        Raylib.CloseWindow();
+    }
+}
